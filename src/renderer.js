318e09b6da8c0296f539b4537feeb0aaedcc09e8
rinoale <rinoale@hanmail.net>
2016-09-22 11:08:43 +0900

    add chat

--- src/renderer.js
+++ src/renderer.js
@@ -2,4 +2,14 @@ const guildMsgPane = document.getElementById('guildMessage');
 
 var ChatNode = require('./nodes/chatNode.js');
 
-guildMsgPane.appendChild(ChatNode.chat('gbsong', 'test'));
+function addChat() {
+    guildMsgPane.appendChild(ChatNode.chat('gbsong', 'test'));
+
+    guildMsgPane.scrollTop = guildMsgPane.scrollHeight;
+}
+
+const testButton = document.getElementById('testButton');
+
+testButton.addEventListener('click', function () {
+    addChat();
+});
